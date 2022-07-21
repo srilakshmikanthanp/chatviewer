299a9096fd4d9a6283ba1ebec4713b105cbd300b
Sri lakshmi kanthan P <srilakshmikanthanp@gmail.com>
2022-07-21 15:42:07 +0530

    add Some Models

---
+++ apps/rest-api/interfaces/models/IUser.ts
@@ -0,0 +1,10 @@
+// Copyright (c) 2022 Sri Lakshmi Kanthan P
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+export default interface IUser {
+  user_id: number;
+  name: string;
+  email: string;
+}
---
+++ apps/rest-api/models/User.ts
@@ -0,0 +1,5 @@
+// Copyright (c) 2022 Sri Lakshmi Kanthan P
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
