4b1876f9c2adadee14e404ffc4dc4f847d896767
Sri lakshmi kanthan P <srilakshmikanthanp@gmail.com>
2022-08-12 23:10:00 +0530

    Add: URl for the Production url to web 👽

--- apps/web/src/constants/index.tsx
+++ apps/web/src/constants/index.tsx
@@ -7,4 +7,6 @@
 export const GOOGLE_CLIENT_ID = "956872187563-n0rjut3g7i7aupstn065gp3oktlec9f0.apps.googleusercontent.com"
 
 // Base URL for the API
-export const BASE_URL = "http://localhost:8000"
+export const BASE_URL = process.env["NODE_ENV"] === "production" ?
+"https://api-chatviewer.herokuapp.com/" :
+"http://localhost:8000";
